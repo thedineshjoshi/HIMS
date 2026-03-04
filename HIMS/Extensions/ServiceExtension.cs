6f5b332287ebd5d008cf8365fd8aa0f4642b8287
thedineshjoshi <9868459709dinesh@gmail.com>
2026-03-04 15:44:13 +0545

    Jwt Middleware implemented

--- HIMS/Extensions/ServiceExtension.cs
+++ HIMS/Extensions/ServiceExtension.cs
@@ -17,6 +17,20 @@ namespace HIMS.Extensions
             services.AddScoped<IStaffService, StaffService>();
             services.AddScoped<IDoctorService, DoctorService>();
             services.AddScoped<ILoginService, LoginService>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        ValidAudience = configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                    };
+                });
             return services;
         }
     }
